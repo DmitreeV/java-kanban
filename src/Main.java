cd4d7169f5b2205ec85fdb7def7ccadf4deec87d
DmitreeV <color.ivanov2016@yandex.ru>
2022-08-20 16:34:53 +0300

    _

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
+import servers.KVServer;
+
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        new KVServer().start();
+    }
+}
+
+
 //import manager.*;
 //import task.*;
 //
--- src/exception/ManagerSaveException.java
+++ src/exception/ManagerSaveException.java
@@ -4,14 +4,6 @@ public class ManagerSaveException extends RuntimeException {
     public ManagerSaveException(String message, Throwable cause) {
         super(message, cause);
     }
-
-    public Throwable getCause(){
-        return super.getCause();
-    }
-
-    public String getMessage() {
-        return super.getMessage();
-    }
 }
 
 
